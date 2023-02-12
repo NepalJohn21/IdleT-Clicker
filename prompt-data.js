31bcfc91305a7d0187aeb55b5ed1b49f0baeecf9
NepalJohn21 <124006434+NepalJohn21@users.noreply.github.com>
2023-02-12 17:56:58 -0300

    allow

--- prompt-data.js
+++ prompt-data.js
@@ -1,5 +1,16 @@
-let itsCreated = false
+if (localStorage.itscreated) {
+    //NO DATA
+  } else {
+    localStorage.setItem("itscreated", 0);
+}
 
-if (!itsCreated) {
-    let getName = prompt
+if (localStorage.itscreated == 0) {
+    let getName = prompt("To get started, Enter a player name:", defaultPlayerName)
+
+    if (getName == null || getName == "") {
+        html_content.innerHTML = '<h1>Fatal Error: Create your player account cancled.</h1>'
+    } else {
+        alert("Subscess!");
+        localStorage.itscreated = 1;
+    }
 }
