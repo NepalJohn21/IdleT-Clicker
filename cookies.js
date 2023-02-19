4ee024b6d33dc1a03a88c149baad30c84648d1ce
NepalJohn21 <124006434+NepalJohn21@users.noreply.github.com>
2023-02-19 15:31:04 -0300

    new cookie

--- cookies.js
+++ cookies.js
@@ -1,3 +1,3 @@
 const d = new Date();
 d.setTime(d.getTime() + (30*24*60*60*1000));
-document.cookie = "username=nepaljohn; expires=" + d + "; path=/";
+document.cookie = "username=nepaljohn; expires=" + d + "; path=/; visits=0; expires=" + d + "; path=/";
